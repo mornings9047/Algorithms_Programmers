d6c3cedadac19b6dd57642fe89f7f8075c6e5ca7
Owner <morningstar9047@gmail.com>
2021-01-07 16:20:19 +0900

    두 개 뽑아서 더하기

--- Level1/src/SumAndSort.java
+++ Level1/src/SumAndSort.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class SumAndSort {
     public int[] solution(int[] numbers) {
-        Set<Integer> set = new HashSet();
+        Set<Integer> set = new HashSet<>();
         for (int i = 0; i < numbers.length - 1; i++)
             for (int j = i + 1; j < numbers.length; j++)
                 set.add(numbers[i] + numbers[j]);
