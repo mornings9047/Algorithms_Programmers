67f8f7f7431b91e409d820ceb1836e49962bd10f
Owner <morningstar9047@gmail.com>
2021-01-07 16:01:39 +0900

    두 개 뽑아서 더하기

---
+++ Level1/src/SumAndSort.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class SumAndSort {
+    public int[] solution(int[] numbers) {
+        Set<Integer> set = new HashSet();
+        for (int i = 0; i < numbers.length - 1; i++)
+            for (int j = i + 1; j < numbers.length; j++)
+                set.add(numbers[i] + numbers[j]);
+
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+        int length = list.size();
+        int[] answer = new int[length];
+        for (int i = 0; i < length; i++)
+            answer[i] = list.get(i);
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        SumAndSort sumAndSort = new SumAndSort();
+        int[] numbers = {2, 1, 3, 4, 1};
+        for (int i : sumAndSort.solution(numbers))
+            System.out.print(i + " ");
+        System.out.println();
+        int[] numbers2 = {5, 0, 2, 7};
+        for (int i : sumAndSort.solution(numbers2))
+            System.out.print(i + " ");
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Programmers Level1
 #### 01.03
 ##### 크레인 인형뽑기 게임
+<https://velog.io/@morningstar/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-01>
 #### 01.04
 ##### 완주하지 못한 선수
+<https://velog.io/@morningstar/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-02>
+#### 01.07
+##### 두 개 뽑아서 더하기
+<https://velog.io/@morningstar/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-03>
