fcc22611410b23379f7c7e3cbe8793c52e0d7d7c
Owner <morningstar9047@gmail.com>
2021-01-19 17:59:36 +0900

    약수의 합

--- Programmers/src/level01/Factor.java
+++ Programmers/src/level01/Factor.java
@@ -3,8 +3,6 @@ package level01;
 public class Factor {
     public int solution(int n) {
         int sum = 0;
-        System.out.println(Math.sqrt(n));
-
         for (int i = 1; i <= Math.sqrt(n); i++) {
             if (n % i == 0) {
                 sum += i;
