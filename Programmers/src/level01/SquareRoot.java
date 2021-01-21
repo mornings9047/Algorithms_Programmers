b6ac896623630a677f01ce5e8b0d1c2fce7fcfea
Owner <morningstar9047@gmail.com>
2021-01-21 13:45:23 +0900

    정수 제곱근 판별

--- Programmers/src/level01/SquareRoot.java
+++ Programmers/src/level01/SquareRoot.java
@@ -3,15 +3,17 @@ package level01;
 public class SquareRoot {
     public long solution(long n) {
         long sqrt = (long) Math.sqrt(n);
-        return Math.pow(sqrt, 2) == n ? sqrt : -1;
+        return (long) Math.pow(sqrt, 2) == n ? (long) Math.pow(sqrt + 1, 2) : -1;
     }
 
     public static void main(String[] args) {
         long n = 121;
         long n2 = 3;
+        long n3 = 1000000000000L;
 
         SquareRoot squareRoot = new SquareRoot();
         System.out.println(squareRoot.solution(n));
         System.out.println(squareRoot.solution(n2));
+        System.out.println(squareRoot.solution(n3));
     }
 }
