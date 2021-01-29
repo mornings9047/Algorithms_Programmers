f25c6d1f391098b56da9c524bd29135bc4d992f9
Owner <morningstar9047@gmail.com>
2021-01-29 19:30:38 +0900

    [1차] 다트 게임

---
+++ Programmers/src/level01/Dart.java
@@ -0,0 +1,104 @@
+package level01;
+
+public class Dart {
+    private String dartResult;
+    private int[] scores;
+    private int scoreIndex;
+    private int sum;
+
+    public Dart() {
+        scores = new int[3];
+        scoreIndex = 0;
+        sum = 0;
+    }
+
+    public int solution(String dartResult) {
+        this.dartResult = dartResult;
+        while (scoreIndex != 3)
+            calculateScore();
+        return sum();
+    }
+
+    public void calculateScore() {
+        setScore();
+        setBonus();
+        if (isOptionValid())
+            setOption();
+        scoreIndex++;
+    }
+
+    public void setScore() {
+        try {
+            scores[scoreIndex] = Integer.parseInt(dartResult.substring(0, 2));
+            dartResult = dartResult.substring(2);
+        } catch (NumberFormatException e) {
+            scores[scoreIndex] = Integer.parseInt(dartResult.substring(0, 1));
+            dartResult = dartResult.substring(1);
+        }
+    }
+
+    public void setBonus() {
+        char bonus = dartResult.charAt(0);
+        if (bonus == 'D')
+            scores[scoreIndex] = (int) Math.pow(scores[scoreIndex], 2);
+        else if (bonus == 'T')
+            scores[scoreIndex] = (int) Math.pow(scores[scoreIndex], 3);
+        dartResult = dartResult.substring(1);
+    }
+
+    public boolean isOptionValid() {
+        if (dartResult.isEmpty())
+            return false;
+        char option = dartResult.charAt(0);
+        return option == '*' || option == '#';
+    }
+
+    public void setOption() {
+        char option = dartResult.charAt(0);
+        if (option == '*') {
+            scores[scoreIndex] *= 2;
+            if (scoreIndex != 0)
+                scores[scoreIndex - 1] *= 2;
+        } else
+            scores[scoreIndex] = -scores[scoreIndex];
+        dartResult = dartResult.substring(1);
+    }
+
+    public int sum() {
+        for (int score : scores)
+            sum += score;
+        return sum;
+    }
+
+    public void reset() {
+        scores = new int[3];
+        scoreIndex = 0;
+        sum = 0;
+    }
+
+    public static void main(String[] args) {
+        String dartResult = "1S2D*3T";
+        String dartResult2 = "1D2S#10S";
+        String dartResult3 = "1D2S0T";
+        String dartResult4 = "1S*2T*3S";
+        String dartResult5 = "1D#2S*3S";
+        String dartResult6 = "1T2D3D#";
+        String dartResult7 = "1D2S3T*";
+
+        Dart dart = new Dart();
+        System.out.println(dart.solution(dartResult));
+        dart.reset();
+        System.out.println(dart.solution(dartResult2));
+        dart.reset();
+        System.out.println(dart.solution(dartResult3));
+        dart.reset();
+        System.out.println(dart.solution(dartResult4));
+        dart.reset();
+        System.out.println(dart.solution(dartResult5));
+        dart.reset();
+        System.out.println(dart.solution(dartResult6));
+        dart.reset();
+        System.out.println(dart.solution(dartResult7));
+        dart.reset();
+    }
+}
--- README.md
+++ README.md
@@ -118,3 +118,5 @@
 <https://velog.io/@morningstar/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-38>
 #### [1차] 비밀지도
 <https://velog.io/@morningstar/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-39>
+#### [1차] 다트 게임
+<https://velog.io/@morningstar/%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-40>
